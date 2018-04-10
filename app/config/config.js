0c86da0821a031793d56997bc64537a518bcd862
MrFlyingChip <kv_l@ukr.net>
2018-04-10 16:32:59 +0300

    new coinApi key

--- app/config/config.js
+++ app/config/config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  apiCoin: '76C22A1B-32F2-45CD-A2B5-AEA08A067075',
+  apiCoin: '96A39D70-DD91-41CD-9382-E0ACA83B9034',
   apiBot: 'https://api.telegram.org/bot571455368:AAF65ScR2kTNEvt9rLqRSrH5N3roZaR6sC8/'
 };
